86aab27210d52c88a0495e1dab6e79f1e27eb4e7
Luke Graham <lgraham04@protonmail.com>
2019-01-13 21:18:03 -0800

    First Commit

---
+++ Chromium Installer & Updater/Chromium Installer & Updater.ps1
@@ -0,0 +1,22 @@
+﻿param([switch]$Elevated)
+function Check-Admin {
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+if ((Check-Admin) -eq $false)  {
+if ($elevated)
+{
+# could not elevate, quit
+}
+ 
+else {
+ 
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+}
+exit
+}
+wget https://download-chromium.appspot.com/dl/Win_x64?type=snapshots -Outfile "C:/ Chromium.zip"
+Expand-Archive -LiteralPath 'C:\ Chromium.zip' -DestinationPath 'C:\Program Files (x86)\Chromium'
+New-Item -ItemType SymbolicLink -Path "C:/Users/Public/Desktop" -Name "Chromium" -Value "C:\Program Files (x86)\Chromium\chrome-win\chrome.exe"
+Copy-Item -Path 'C:\Chromium Installer & Updater\Chromium.lnk' -Destination "$home\AppData\Roaming\Microsoft\Windows\Start Menu\Programs"
+Remove-Item 'C:\ Chromium.zip'
---
+++ Chromium Installer & Updater/README.txt
@@ -0,0 +1,6 @@
+MAKE SURE YOU EXTRACT THIS FOLDER TO YOUR C: DRIVE OR IT WILL NOT WORK
+DO NOT DELETE THE FILE Chromium DOING SO WILL MESS UP THE INSTALLATION AND UPDATE
+Instructions:
+----------------
+1. Run Chromium Updater Script
+2. Launch Chromium
